0db6cb68ddf1badfc1da72ef6cc39dc0457a2357
awadalla <awadalla@cs.washington.edu>
2022-10-22 20:22:38 -0700

    cut down time by half

--- Backend/productsearch.py
+++ Backend/productsearch.py
@@ -1,21 +1,20 @@
 # Imports
-import urllib.urlopen as open_url
+import urllib.request
 from bs4 import BeautifulSoup as bs
 import itertools
 from googlesearch import search
 
 # retrieve links from query
 def get_links(query):
-  return search(query, tld="co.in", num=3, stop=3, pause=2)
+  return search(query, tld="com", num=3, stop=3, pause=0)
 
 def get_products(query):
   links = get_links(query)
   objs = []
   for i in itertools.islice(links, 3):
-    content = open_url(i)
-    content = bs(content)
-    title = content.find('title')
-    content.find('')
+    content = urllib.request.urlopen(i).read()
+    content = bs(content, features="html.parser")
+    title = content.find('title').text
     objs.append({
         "title": title,
         "link": i
