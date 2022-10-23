0889ebd46447d978b3d3c59d52d883cacff82ba2
Zyad <42823027+zyadcodes@users.noreply.github.com>
2022-10-22 17:28:16 -0700

    added correct endpoint

--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,39 @@
+# To run: flask --app main run
+
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+@app.route("/generate", methods=["POST"])
+def _generate():
+
+    data = request.get_json()
+
+    shirt = data["shirt"]
+    pants = data["pants"]
+    image = data["image"]
+
+    # Post your code here
+
+    return jsonify({}), 200
+
+
+@app.route("/get_products", methods=["POST"])
+def _get_products():
+
+    data = request.get_json()
+
+    shirt = data["shirt"]
+    pants = data["pants"]
+
+    # Post your code here
+
+
+
+    return jsonify({}), 200
 
 
 if __name__ == "__main__":
